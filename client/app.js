2a34b5ac709d0776fe18b7a166db53c77180c836
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-02-19 11:34:14 +0100

    Add app.js and set references to html elements

---
+++ client/app.js
@@ -0,0 +1,20 @@
+'use strict';
+{
+  const opts = {
+    loginForm: '#welcome-form',
+    messagesSection: '#messages-section',
+    messagesList: '#messages-list',
+    addMessageForm: '#add-messages-form',
+    userNameInput: '#username',
+    messageContentInput: '#message-content',
+  };
+
+  const loginForm = document.querySelector(opts.loginForm);
+  const messagesSection = document.querySelector(opts.messagesSection);
+  const messagesList = document.querySelector(opts.messagesList);
+  const addMessageForm = document.querySelector(opts.addMessageForm);
+  const userNameInput = document.querySelector(opts.userNameInput);
+  const messageContentInput = document.querySelector(opts.messageContentInput);
+  const userName = '';
+
+}
