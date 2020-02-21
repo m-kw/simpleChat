60484787f5919ce7c4670273760caf0ad3cd7ec3
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-02-21 12:35:51 +0100

    Add date to messages, add a possibility to remove a message

--- client/app.js
+++ client/app.js
@@ -1,9 +1,9 @@
 'use strict';
 {
   const socket = io();
-  socket.on('message', ({ author, content }) => addMessage(author, content));
-  socket.on('newUser', (user) => addMessage('Chat Bot', user + ' has joined the converstaion!'));
-  socket.on('removeUser', (user) => addMessage('Chat Bot', user + ' has left the conversation... :('));
+  socket.on('message', ({ author, date, content }) => addMessage(author, date, content));
+  socket.on('newUser', (user) => addMessage('Chat Bot', null, user + ' has joined the converstaion!'));
+  socket.on('removeUser', (user) => addMessage('Chat Bot', null, user + ' has left the conversation... :('));
 
   const opts = {
     loginForm: '#welcome-form',
@@ -36,20 +36,31 @@
     }
   }
 
-  function addMessage(author, content) {
+  function addMessage(author, date, content) {
     const message = document.createElement('li');
 
     const heading = document.createElement('h3');
     heading.classList.add('message__author');
     if (author === userName) {
-      heading.innerHTML = 'You'
+      heading.innerHTML = 'You';
     } else {
       heading.innerHTML = author;
     }
 
+    const dateField = document.createElement('div');
+    dateField.classList.add('message__date');
+    dateField.innerHTML = date;
+
     const div = document.createElement('div');
     div.classList.add('message__content');
     div.innerHTML = content;
+
+    const close = document.createElement('div');
+    close.classList.add('message__close');
+    close.innerHTML = 'X';
+    close.addEventListener('click', () => message.remove());
+
+    div.appendChild(close);
 
     message.classList.add('message', 'message--received');
     if (author === userName) {
@@ -57,6 +68,7 @@
     }
 
     message.appendChild(heading);
+    message.appendChild(dateField);
     message.appendChild(div);
 
     messagesList.appendChild(message);
@@ -68,12 +80,21 @@
 
     const messageContent = messageContentInput.value;
 
+    const today = new Date();
+    const day = (today.getDate()).toString().padStart(2, 0);
+    const month = (today.getMonth() + 1).toString().padStart(2, 0);
+    const year = today.getFullYear();
+    const time = today.getTime() / 1000;
+    const minutes = Math.floor((time / 60) % 60).toString().padStart(2, 0);
+    const hours = Math.floor((time / 3600) % 24).toString().padStart(2, 0);
+    const date = `${day}.${month}.${year} ${hours}:${minutes}`;
+
     if (!messageContent.length) {
       alert('You can\'t send empty message');
       return;
     } else {
-      addMessage(userName, messageContent);
-      socket.emit('message', { author: userName, content: messageContent })
+      addMessage(userName, date, messageContent);
+      socket.emit('message', { author: userName, date: date, content: messageContent })
       messageContentInput.value = '';
     }
   }
